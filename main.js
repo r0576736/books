aafd377c6a746a0bef6dd5683cf1f2786eb73eb2
astrid thijs <r0576736@student.thomasmore.be>
2016-12-21 17:18:37 +0100

    insertBook werkt!!!!

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 var express = require("express"); //eenvoudige webserver in node js
 var parser = require ("body-parser"); //externe op express voor eenvoudig body uit te lezen
 
+var dal = require('./storage.js') //de main wil de dal vragen aan storage.js
+
 var app = express();
 app.use(parser.json());
 
@@ -9,11 +11,12 @@ app.get('/', function(request, response){
         response.send("Hello World");
         });
 
-var teller = 3;
+var teller = 3; //teller voor ID
 
 
 var Book = function(id, name){
     this.id = id;
+//    this_id = id;     dit zorgt ervoor dat je uw eigen id kan kiezen.
     this.name = name;
     };
     
@@ -24,9 +27,10 @@ app.get('/books', function(request, response){
         });
 
 app.post('/books', function(request, response){
-    var book = new Book(teller++, request.body.name); //"naam" : "De prinses"
-    books.push(book);
-    response.status(201).send();
+    var book = new Book(teller++, request.body.name);   //"naam" : "De prinses" teller voor ID ++
+    dal.insertBooks(book, function(){                   //functie zie storage.js
+        response.status(201).send();
+    }) ;          
 });
 
 console.log("Hello World");
